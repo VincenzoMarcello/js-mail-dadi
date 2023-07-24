dea1dac66218cc39425edee678fc00e2e6970ad4
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-07-24 16:32:35 +0200

    feat: add exercise two complete

--- Dadi/main.js
+++ Dadi/main.js
@@ -1,1 +1,23 @@
-console.log("js ok");
+const max = 6;
+const min = 1;
+
+//* Giocatore 1 lancia il dado
+const gamerOneValue = Math.floor(Math.random() * max) + min;
+alert("il Giocatore 1 ha ottenuto " + gamerOneValue);
+
+//* Giocatore 2 lancia il dado
+const gamerTwoValue = Math.floor(Math.random() * max) + min;
+alert("il Giocatore 2 ha ottenuto " + gamerTwoValue);
+
+//* SE Giocatore 1 ha un numero superiore di giocatore 2 vince
+if (gamerOneValue > gamerTwoValue) {
+  alert("Giocatore 1 Win");
+}
+//* ALTRIMENTI SE Giocatore 2 ha un numero superiore di giocatore 1 vince
+else if (gamerTwoValue > gamerOneValue) {
+  alert("Giocatore 2 Win");
+}
+//* ALTRIMENTI essendo uguali abbiamo un pareggio
+else {
+  alert("Pareggio");
+}
